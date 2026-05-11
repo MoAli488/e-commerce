57ce628d1c85ea7f4fd94eb66b2ed0ccac23ca32
Mohamed Ali <mohamed.ali15122004@gmail.com>
2026-05-11 06:22:30 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+console.log("Hellsso from typescript");
+let app: number = 10;
+if (app < 20){
+    app += 20;
+}
+
+function abc(b: number) {
+    console.log(typeof b)
+}
+
+let user: [number, string] = [1, "ss"];
+user.push(2);
+
+abc(app);
